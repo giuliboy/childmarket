a25efe1c3544d79fb5fa08db5b5d4a25a528fc0f
De Gani Giuliano <gdegani@hsr.ch>
2017-11-02 14:10:02 +0100

    isSold UI improvements

--- KinderArtikelBoerse/App.xaml.cs
+++ KinderArtikelBoerse/App.xaml.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Markup;
 
 namespace KinderArtikelBoerse
 {
@@ -13,5 +16,20 @@ namespace KinderArtikelBoerse
     /// </summary>
     public partial class App : Application
     {
+        static App()
+        {
+            FrameworkElement.LanguageProperty.OverrideMetadata(
+                typeof( FrameworkElement ),
+                new FrameworkPropertyMetadata(
+                    XmlLanguage.GetLanguage( CultureInfo.CurrentCulture.IetfLanguageTag ) ) );
+        }
+
+        protected override void OnStartup( StartupEventArgs e )
+        {
+            base.OnStartup( e );
+            CultureInfo culture = new CultureInfo( ConfigurationManager.AppSettings["DefaultCulture"] );
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+        }
     }
 }
--- KinderArtikelBoerse/Utils/DemoMarketDataProvider.cs
+++ KinderArtikelBoerse/Utils/DemoMarketDataProvider.cs
@@ -10,8 +10,17 @@ namespace KinderArtikelBoerse.Utils
         public IEnumerable<Item> Items => new List<Item>()
         {
             new Item() { ItemIdentifier="AB1", Description="roter Pulli", Price=5.0f , Size="36/38", Seller= Sellers.Last()},
-            new Item() { ItemIdentifier="AB2", Description="lego ", Price=2.5f, Seller = Sellers.First() },
-            new Item() { ItemIdentifier="AB3", Description="jacke rot", Price=15f , Size="112", Seller=Sellers.Skip(1).First()},
+            new Item() { ItemIdentifier="AB2", Description="Pulover", Price=5.0f , Size="38", Seller= Sellers.Last()},
+            new Item() { ItemIdentifier="AB3", Description="jojo", Price=5.5f , Seller= Sellers.Last()},
+            new Item() { ItemIdentifier="AB4", Description="puzzli", Price=25.0f , Seller= Sellers.Last()},
+
+            new Item() { ItemIdentifier="C1", Description="buch ", Price=2.5f, Seller = Sellers.First() },
+            new Item() { ItemIdentifier="C2", Description="bobby car ", Price=2.5f, Seller = Sellers.First() },
+            new Item() { ItemIdentifier="C3", Description="ente", Price=2.5f, Seller = Sellers.First() },
+
+            new Item() { ItemIdentifier="CD1", Description="jacke blau", Price=11f , Size="108", Seller=Sellers.Skip(1).First()},
+            new Item() { ItemIdentifier="CD2", Description="socken", Price=1f , Size="112", Seller=Sellers.Skip(1).First()},
+
         };
 
         public IEnumerable<Seller> Sellers => new List<Seller>()
